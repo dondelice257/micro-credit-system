b297c2d3c2476fb63022012577dd685ff5e4b5c6
dondelice257 <dondelicedushime@gmail.com>
2024-04-18 17:15:31 +0200

    update pre-commit

--- apps/credit/models/micro_credit_type.py
+++ apps/credit/models/micro_credit_type.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
 
 class MicroCreditType(models.Model):
-  slug = models.CharField(max_length=255)
-  name = models.CharField(max_length=255)
-  mode = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    mode = models.CharField(max_length=255)
